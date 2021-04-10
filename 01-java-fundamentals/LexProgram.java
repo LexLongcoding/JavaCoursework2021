053dc8d2a7eb8487685afbfe1ebc283c8c5ee952
Lex Long <DevLexLong@gmail.com>
2021-04-10 16:49:07 -0400

    Completed "Your Own Java Program"

---
+++ 01-java-fundamentals/LexProgram.java
@@ -0,0 +1,8 @@
+public class LexProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Lex Long");
+        System.out.println("I was born in 1985");
+        System.out.println("I'm from PA");
+
+    }
+}
