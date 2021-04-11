80db38179f2d886672c94320ad336354103b7024
Lex Long <DevLexLong@gmail.com>
2021-04-11 13:08:10 -0400

    Completed Pythagorean Theorem assignment

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,7 @@
+public class Pythagorean {
+    public double calculateHypotenuse (int lenA, int lenB) {
+
+        double legC = Math.sqrt((lenA * lenA ) + (lenB * lenB));
+        return legC;
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest{
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        Pythagorean theorem = new Pythagorean();
+        System.out.println(theorem.calculateHypotenuse(a,b));
+    }
+}
