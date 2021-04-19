be9ba07e82d59045691f1047bfa9ee52419e8869
Lex Long <DevLexLong@gmail.com>
2021-04-19 10:37:37 -0400

    udated

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,9 @@
+public class HumanTest {
+    public static void main(String[] args){
+        Human player1 = new Human();
+        Human player2 = new Human();
+        player1.attack(player2);
+        System.out.println(player2.health);
+    }
+}
+
