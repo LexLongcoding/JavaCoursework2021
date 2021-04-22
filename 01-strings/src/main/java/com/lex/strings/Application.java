ddf44b75f7ad109e146c78844b09dcb315c35fdd
Lex Long <DevLexLong@gmail.com>
2021-04-22 13:36:00 -0400

    update

--- 01-strings/src/main/java/com/lex/strings/Application.java
+++ 01-strings/src/main/java/com/lex/strings/Application.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("unused")
+
 @SpringBootApplication
 @RestController
 public class Application {
