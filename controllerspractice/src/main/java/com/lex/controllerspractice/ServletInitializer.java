8a13ed6c33700ad1217dbc1d7cb628d7e0edfbe4
Lex Long <DevLexLong@gmail.com>
2021-04-22 15:59:27 -0400

    updates

--- controllerspractice/src/main/java/com/lex/controllerspractice/ServletInitializer.java
+++ controllerspractice/src/main/java/com/lex/controllerspractice/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.lex.strings;
+package com.lex.controllerspractice;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		return application.sources(ControllerspracticeApplication.class);
 	}
 
 }
--- controllerspractice/src/test/java/com/lex/controllerspractice/ControllerspracticeApplicationTests.java
+++ controllerspractice/src/test/java/com/lex/controllerspractice/ControllerspracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lex.strings;
+package com.lex.controllerspractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ControllerspracticeApplicationTests {
 
 	@Test
 	void contextLoads() {
