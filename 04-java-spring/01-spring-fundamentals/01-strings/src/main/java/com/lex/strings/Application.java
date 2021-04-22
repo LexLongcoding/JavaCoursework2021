90ef90e52f0e8dc2bb55d1fda52cf51c1e28ae37
Lex Long <DevLexLong@gmail.com>
2021-04-22 16:11:01 -0400

    Commit

---
+++ 04-java-spring/01-spring-fundamentals/01-strings/src/main/java/com/lex/strings/Application.java
@@ -0,0 +1,26 @@
+package com.lex.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@RequestMapping("/")
+	public String msg1() {
+		return "Greetings user!";
+
+	}
+	@RequestMapping("/random")
+	public String msg2() {
+		return "Spring boot makes Java easier";
+
+	}
+}
---
+++ 04-java-spring/01-spring-fundamentals/01-strings/src/test/java/com/lex/strings/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex.strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
