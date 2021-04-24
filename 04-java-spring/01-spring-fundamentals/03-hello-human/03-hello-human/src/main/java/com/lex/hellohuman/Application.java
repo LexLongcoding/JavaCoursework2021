c0cab8b99db2dcf54f787aeb676dd9be7256eefe
Lex Long <DevLexLong@gmail.com>
2021-04-24 09:10:46 -0400

    updated

--- 04-java-spring/01-spring-fundamentals/03-hello-human/03-hello-human/src/main/java/com/lex/hellohuman/Application.java
+++ 04-java-spring/01-spring-fundamentals/03-hello-human/03-hello-human/src/main/java/com/lex/hellohuman/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloHumanApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloHumanApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ 04-java-spring/01-spring-fundamentals/03-hello-human/03-hello-human/src/main/java/com/lex/hellohuman/HelloHuman.java
@@ -0,0 +1,14 @@
+package com.lex.hellohuman;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHuman {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHuman.class, args);
+	}
+
+}
+
+
