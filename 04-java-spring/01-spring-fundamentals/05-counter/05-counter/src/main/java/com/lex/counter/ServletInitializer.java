010318ea3e01af9017ab77c3224bb7ae3cb750d1
Lex Long <DevLexLong@gmail.com>
2021-04-25 11:01:12 -0400

    updates

---
+++ 04-java-spring/01-spring-fundamentals/05-counter/05-counter/src/main/java/com/lex/counter/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.lex.counter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CounterApplication.class);
+	}
+
+}
+
---
+++ 04-java-spring/01-spring-fundamentals/05-counter/05-counter/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Counter</title>
+</head>
+<body>
+	<h1>Counter</h1>
+	<a href="/counter">Visit Counter Results</a>
+</body>
+</html>
